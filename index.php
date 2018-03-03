dd8eae880d7b1fed0963d811283ab282d3a7683c
Kaila Funaki <yfunaki@csumb.edu>
2018-03-03 23:45:00 +0000

    Lab 3 Done

--- index.php
+++ index.php
@@ -1,8 +1,41 @@
 <?php
+session_start(); 
+$_SESSION['timer'] = microtime() * 1000;
+$names = array("Rick", "Morty", "Mr. Meeseeks", "Mr. Poopybutthole");
+shuffle($names);
+
+    $players = array();
+    $scores = array();
+    $cards = array();
+
+function getPlayers($num){
+        global $names, $players;
+        
+        $players = array_rand($names, $num);
+    }
+
 function getHand(){
         global $names, $players, $scores, $cards;
         for($i = 0; $i < count($players); $i++){
             echo  "<h2>" . $names[$players[$i]] . "<h2/>";
+            
+            if($names[$players[$i]] == "Rick")
+            {
+                echo "<img id = 'playerImg' src = 'img/rick.png' alt = 'Rick' />";
+            }
+            else if($names[$players[$i]] == "Morty")
+            {
+                echo "<img id = 'playerImg' src = 'img/morty.png' alt = 'Morty' />";
+            }
+            else if($names[$players[$i]] == "Mr. Meeseeks")
+            {
+                echo "<img id = 'playerImg' src = 'img/mrmeeseeks.png' alt = 'Mr. Meeseeks' />";
+            }
+            else if($names[$players[$i]] == "Mr. Poopybutthole")
+            {
+                echo "<img id = 'playerImg' src = 'img/mrpoopy.png' alt = 'Mr. Poopybutthole' />";
+            }
+            
             $total = 0;
             while($total < 42){
                 $card = rand(1,52);
@@ -11,6 +44,9 @@ function getHand(){
                 }
                 if(($total+(($card%13 == 0 ? 13 : $card%13))) <= 42){
                     $total += $card % 13;
+                    if($card%13 == 0){
+                           $total+=13;
+                        }
                     if($card < 14){
                         echo "<img src='img/cards/clubs/" . $card . ".png'>";
                     }
@@ -36,9 +72,98 @@ function getHand(){
                 }
             }
             $scores[] = $total;
-            echo "<h1>"."<span style=color:red>".$total ."</span>". "<h1/>";
+            echo "<h2>"."<span style=color:red>"."Score: "."$total"."</span>"."<h2/>";
             echo "<hr> <hr/>";
             echo "<br>";
         }
     }
+    function getWinner(){
+    global $players,$names, $scores;
+    $winner =  array ();
+    $dontAddScore = array();
+    $scorewinner  = $scores[0]; 
+    for($i =0;$i <4; $i++){
+         if($scorewinner==$scores[$i]){
+            array_push($winner,$names[$i]);
+        }
+        
+        else if($scorewinner<$scores[$i]){
+            if(count($winner)>1){
+                $winner = array();
+               
+            }
+             $scorewinner = $scores[$i];
+           $replacement = array(0=>$names[$i]);
+            $winner = array_replace($winner,$replacement);
+        }
+       
+        
+    }
+$winningscore = 0;
+for($i=0;$i < 4;$i++){
+    for($ii = 0; $ii<=count($winner);$ii++){
+    if($names[$i]==$winner[$ii]){
+        $winningscore = $winningscore + $scores[$i];
+        
+    }
+    }
+}
+
+$pointsEarned = 0;
+for($i = 0;$i < 4; $i++){
+    $pointsEarned = $pointsEarned + $scores[$i];
+}
+$pointsEarned = $pointsEarned-$winningscore;
+ $pointsEarned = $pointsEarned/count($winner);
+       
+         for($i=0; $i <= count($winner); $i++)
+         { 
+            echo "<h1> $winner[$i] ";
+         }
+         echo " wins $pointsEarned points! </h1>";
+    }
+
 ?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Lab 4: Rick and Morty Silver Jack </title>
+        <meta charset="utf-8" />
+        <link href="https://fonts.googleapis.com/css?family=Luckiest+Guy" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Candal" rel="stylesheet">
+        <style>
+            @import url("css/styles.css");
+            
+        </style>
+    </head>
+    <body>
+        
+        <header>
+            <img id = "title" src = "img/rick-and-morty-silver-jack.png" alt = "Rick and Morty Silver Jack">
+        </header>
+        
+        <div id="main">
+    
+            <?php
+                getPlayers(4);
+                getHand();
+                getWinner();
+                $_SESSION['timer2'] = microtime() * 1000;
+            ?>
+
+        </div>
+        
+            <form>
+                <input type = "submit" value = "Play Again?">
+            </form>
+        
+    </body>
+    
+    <footer>
+        <br/><br/><br/>
+        CST 336 Internet Programming 2018&copy; Funaki, Martinez, Peppmuller, Vucinich <br />
+        <strong> Disclaimer: </strong> This information on this webpage is used only for academic purposes. <br />
+        Time taken to load: <?= round (($_SESSION['timer2'] - $_SESSION['timer'])[, int $precision = 4 [, int $mode = PHP_ROUND_HALF_UP ]] )?> seconds <br/>
+    </footer>
+</html>
